d2d9720b688902a9b9b7a165505c313b049ef805
zhouxia <xiazhouzero@gmail.com>
2022-01-30 17:50:29 +0000

    fix path error

--- experiments/figure11/fetch_policies.sh
+++ experiments/figure11/fetch_policies.sh
@@ -6,10 +6,10 @@ mkdir -p ./policies/ACES
 # Camera_To_USBDisk
 echo "[+] Camera_To_USBDisk: OPEC"
 mkdir -p ./policies/OPEC/Camera_To_USBDisk
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/Camera_To_USBDisk_final_policy.json ./policies/OPEC/Camera_To_USBDisk
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/Camera_To_USBDisk
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/Camera_To_USBDisk
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/Camera_To_USBDisk
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/Camera_To_USBDisk_final_policy.json ./policies/OPEC/Camera_To_USBDisk
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/Camera_To_USBDisk
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/Camera_To_USBDisk
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/Camera_To_USBDisk
 
 # PinLock
 echo "[+] PinLock: OPEC"
@@ -24,38 +24,38 @@ cp ${OI_DIR}stm32f407/PinLock/Decode/SW4STM32/STM32F4-DISCO/build9/func_memdep_g
 # FatFs_uSD
 echo "[+] FatFs_uSD: OPEC"
 mkdir -p ./policies/OPEC/FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/FatFs_uSD_operation_subfunc.json ./policies/OPEC/FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/FatFs_uSD_final_policy.json ./policies/OPEC/FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/FatFs_uSD
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/FatFs_uSD_operation_subfunc.json ./policies/OPEC/FatFs_uSD
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/FatFs_uSD_final_policy.json ./policies/OPEC/FatFs_uSD
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/FatFs_uSD
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/FatFs_uSD
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/FatFs_uSD
 
 
 # LCD_AnimatedPictureFromSDCard
 echo "[+] LCD_AnimatedPictureFromSDCard: OPEC"
 mkdir -p ./policies/OPEC/LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_AnimatedPictureFromSDCard_operation_subfunc.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_AnimatedPictureFromSDCard_final_policy.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_AnimatedPictureFromSDCard_operation_subfunc.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_AnimatedPictureFromSDCard_final_policy.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LCD_AnimatedPictureFromSDCard
 
 
 # LCD_PicturesFromSDCard
 echo "[+] LCD_PicturesFromSDCard: OPEC"
 mkdir -p ./policies/OPEC/LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_PicturesFromSDCard_operation_subfunc.json ./policies/OPEC/LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_PicturesFromSDCard_final_policy.json ./policies/OPEC/LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LCD_PicturesFromSDCard
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_PicturesFromSDCard_operation_subfunc.json ./policies/OPEC/LCD_PicturesFromSDCard
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/LCD_PicturesFromSDCard_final_policy.json ./policies/OPEC/LCD_PicturesFromSDCard
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LCD_PicturesFromSDCard
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LCD_PicturesFromSDCard
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LCD_PicturesFromSDCard
 
 
 # LwIP_TCP_Echo_Server
 echo "[+] LwIP_TCP_Echo_Server: OPEC"
 mkdir -p ./policies/OPEC/LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/LwIP_TCP_Echo_Server_operation_subfunc.json ./policies/OPEC/LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/LwIP_TCP_Echo_Server_final_policy.json ./policies/OPEC/LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LwIP_TCP_Echo_Server
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/LwIP_TCP_Echo_Server_operation_subfunc.json ./policies/OPEC/LwIP_TCP_Echo_Server
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/LwIP_TCP_Echo_Server_final_policy.json ./policies/OPEC/LwIP_TCP_Echo_Server
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/all_gv.json ./policies/OPEC/LwIP_TCP_Echo_Server
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/ro_gv.json ./policies/OPEC/LwIP_TCP_Echo_Server
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build9/func_memdep_gv_peri_ptr_bf_operation.json ./policies/OPEC/LwIP_TCP_Echo_Server
--- experiments/figure9/fetch_binary.sh
+++ experiments/figure9/fetch_binary.sh
@@ -10,25 +10,25 @@ cp ${OI_DIR}stm32f407/PinLock/Decode/SW4STM32/STM32F4-DISCO/build4/PinLock--base
 cp ${OI_DIR}stm32f407/PinLock/Decode/SW4STM32/STM32F4-DISCO/build4/PinLock--oi--final.elf ./bins/opec
 
 # Camera_To_USBDisk
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build4/Camera_To_USBDisk--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build4/Camera_To_USBDisk--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build4/Camera_To_USBDisk--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/Camera_To_USBDisk/SW4STM32/STM32469I_EVAL/build4/Camera_To_USBDisk--oi--final.elf ./bins/opec
 
 # FatFs_uSD
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build4/FatFs_uSD--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build4/FatFs_uSD--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build4/FatFs_uSD--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/FatFs_uSD/SW4STM32/STM32469I_EVAL/build4/FatFs_uSD--oi--final.elf ./bins/opec
 
 # LCD_AnimatedPictureFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_AnimatedPictureFromSDCard--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_AnimatedPictureFromSDCard--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_AnimatedPictureFromSDCard--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/LCD_AnimatedPictureFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_AnimatedPictureFromSDCard--oi--final.elf ./bins/opec
 
 # LCD_PicturesFromSDCard
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_PicturesFromSDCard--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_PicturesFromSDCard--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_PicturesFromSDCard--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/LCD_PicturesFromSDCard/SW4STM32/STM32469I_EVAL/build4/LCD_PicturesFromSDCard--oi--final.elf ./bins/opec
 
 # LwIP_TCP_Echo_Server
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build4/LwIP_TCP_Echo_Server--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build4/LwIP_TCP_Echo_Server--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build4/LwIP_TCP_Echo_Server--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/LwIP_TCP_Echo_Server/SW4STM32/STM32469I_EVAL/build4/LwIP_TCP_Echo_Server--oi--final.elf ./bins/opec
 
 # CoreMark
-cp ${OI_DIR}stm32f469I/CoreMark/SW4STM32/STM32469I_EVAL/build4/CoreMark--baseline.elf ./bins/baseline
-cp ${OI_DIR}stm32f469I/CoreMark/SW4STM32/STM32469I_EVAL/build4/CoreMark--oi--final.elf ./bins/opec
+cp ${OI_DIR}stm32469I/CoreMark/SW4STM32/STM32469I_EVAL/build4/CoreMark--baseline.elf ./bins/baseline
+cp ${OI_DIR}stm32469I/CoreMark/SW4STM32/STM32469I_EVAL/build4/CoreMark--oi--final.elf ./bins/opec
